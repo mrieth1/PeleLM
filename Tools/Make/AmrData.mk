5d2f35ffad27b013a814e644fd0e717c9d3b60ad
Marc Day <MSDay@lbl.gov>
2018-10-19 19:34:46 -0700

    Clean out unneeded files from gh-pages branch

--- Tools/Make/AmrData.mk
+++
@@ -1,4 +0,0 @@
-DEFINES += -DBL_USE_VELOCITY -DBL_NOLINEVALUES
-CEXE_headers += DataServices.H AmrData.H XYPlotDataList.H AmrvisConstants.H
-CEXE_sources += DataServices.cpp AmrData.cpp
-FEXE_sources += FABUTIL_$(DIM)D.F
--- Tools/Make/flucts.mk
+++
@@ -1,3 +0,0 @@
-FEXE_sources += FLUCTFILE.F INFL_FORCE_$(DIM)D.F INFL_FORCE_F.F
-CEXE_sources += inflow.cpp
-
